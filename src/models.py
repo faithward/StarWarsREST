8be2b4464a2ea171ed60dfae56bbfee0c974b244
faithward <faithward24@gmail.com>
2022-11-09 20:02:26 +0000

    fixed models

--- src/models.py
+++ src/models.py
@@ -1,31 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
-
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
     def __repr__(self):
         return '<User %r>' % self.username
-
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
-
 class People(db.Model):
     __tablename__ = 'people'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
     birth_year = db.Column(db.String(10), unique=False, nullable=False)
     homeworld = db.Column(db.String(50), unique=False, nullable=False)
-
+    FavChar = db.relationship('FavChar', lazy=True)
     def serialize(self):
         return {
             "id" : self.id,
@@ -33,6 +31,17 @@ class People(db.Model):
             "birth_year" : self.birth_year,
             "homeworld" : self.homeworld
         }
+class FavChar(db.Model):
+    __tablename__ = 'favPerson'
+    id = db.Column(db.Integer, primary_key=True)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name
+        }
+
 
 class Planet(db.Model):
     __tablename__ = 'planet'
@@ -40,7 +49,7 @@ class Planet(db.Model):
     name = db.Column(db.String(50), unique=True, nullable=False)
     diameter = db.Column(db.String(15), nullable=False)
     population = db.Column(db.String(18), nullable=False)
-
+    FavPlanet = db.relationship('FavPlanet', lazy=True)
     def serialize(self):
         return {
             "id" : self.id,
@@ -48,6 +57,14 @@ class Planet(db.Model):
             "diameter" : self.diameter,
             "population" : self.population
         }
-
+class FavPlanet(db.Model):
+    __tablename__ = 'favPlanet'
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name
+        }
 
 
